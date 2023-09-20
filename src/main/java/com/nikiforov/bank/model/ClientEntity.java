248e986e844d8ea05c08e8f8a9a3ec89e829649b
Nikiforov <nomail@gmail.com>
2023-09-20 14:30:11 +0300

    develop start

---
+++ src/main/java/com/nikiforov/bank/model/ClientEntity.java
@@ -0,0 +1,25 @@
+package com.nikiforov.bank.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="client")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientEntity {
+
+    @SequenceGenerator(name= "test",sequenceName = "client_id_seq",allocationSize = 1)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "test")
+    private Long id;
+    private String fio;
+}
